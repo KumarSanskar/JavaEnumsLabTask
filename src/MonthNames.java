cf862e58412ce30f0173894e030f123371294376
Kumar Sanskar <kumar.sanskar_cs19@gla.ac.in>
2020-10-27 21:17:00 +0530

    add field and arguments numberOfDaysInMonth

--- src/MonthNames.java
+++ src/MonthNames.java
@@ -1,3 +1,12 @@
 public enum MonthNames {
     JANUARY, FEBRUARY, MARCH, APRIL, MAY, JUNE, JULY, AUGUST, SEPTEMBER, OCTOBER, NOVEMBER, DECEMBER;
+
+
+    private final int numberOfDaysInMonth;
+
+    MonthNames(int numberOfDaysInMonth) {
+        this.numberOfDaysInMonth = numberOfDaysInMonth;
+    }
 }
+
+
