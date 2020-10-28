e94fce8f122890a3b394191ef955a7a2bcf9d487
Kumar Sanskar <kumar.sanskar_cs19@gla.ac.in>
2020-10-28 12:43:30 +0530

    refactored my code using enhanced switch statement and made it more user friendly.

--- src/MonthNames.java
+++ src/MonthNames.java
@@ -1,5 +1,4 @@
-import java.util.Calendar;
-import java.util.GregorianCalendar;
+import java.util.Scanner;
 
 public enum MonthNames {
     JANUARY(31), FEBRUARY(29), MARCH(31), APRIL(30), MAY(31), JUNE(30), JULY(31), AUGUST(31), SEPTEMBER(30), OCTOBER(30), NOVEMBER(30), DECEMBER(31);
@@ -18,10 +17,30 @@ public enum MonthNames {
 
 class Main {
     public static void main(String[] args) {
-        int presentDateOfMonth = new GregorianCalendar().get(Calendar.DAY_OF_MONTH);
-        int numberOfDaysInMonth = new GregorianCalendar().getActualMaximum(Calendar.DAY_OF_MONTH);
-        int numberOfDaysLeft = numberOfDaysInMonth - presentDateOfMonth;
-        System.out.println("There are " + numberOfDaysLeft + " left in present month.");
+        System.out.println("Please enter name of current month for year 2020: ");
+        Scanner scanner = new Scanner(System.in);
+        String name = scanner.nextLine().trim().toUpperCase();
+        System.out.println("Please enter today's date: ");
+        int presentDate = scanner.nextInt();
+        scanner.close();
+        System.out.println("You entered month name as " + name + " and present date as " + presentDate);
+        MonthNames currentMonth = MonthNames.valueOf(name);
+        switch (currentMonth) {
+            case JANUARY -> System.out.println("You have " + (MonthNames.JANUARY.getNumberOfDaysInMonth() - presentDate) + " days left in month of January.");
+            case FEBRUARY -> System.out.println("You have " + (MonthNames.FEBRUARY.getNumberOfDaysInMonth() - presentDate) + " days left in month of February.");
+            case MARCH -> System.out.println("You have " + (MonthNames.MARCH.getNumberOfDaysInMonth() - presentDate) + " days left in month of March.");
+            case APRIL -> System.out.println("You have " + (MonthNames.APRIL.getNumberOfDaysInMonth() - presentDate) + " days left in month of April.");
+            case MAY -> System.out.println("You have " + (MonthNames.MAY.getNumberOfDaysInMonth() - presentDate) + " days left in month of May.");
+            case JUNE -> System.out.println("You have " + (MonthNames.JUNE.getNumberOfDaysInMonth() - presentDate) + " days left in month of June.");
+            case JULY -> System.out.println("You have " + (MonthNames.JULY.getNumberOfDaysInMonth() - presentDate) + " days left in month of July.");
+            case AUGUST -> System.out.println("You have " + (MonthNames.AUGUST.getNumberOfDaysInMonth() - presentDate) + " days left in month of August.");
+            case SEPTEMBER -> System.out.println("You have " + (MonthNames.SEPTEMBER.getNumberOfDaysInMonth() - presentDate) + " days left in month of September.");
+            case OCTOBER -> System.out.println("You have " + (MonthNames.OCTOBER.getNumberOfDaysInMonth() - presentDate) + " days left in month of October.");
+            case NOVEMBER -> System.out.println("You have " + (MonthNames.NOVEMBER.getNumberOfDaysInMonth() - presentDate) + " days left in month of November.");
+            case DECEMBER -> System.out.println("You have " + (MonthNames.DECEMBER.getNumberOfDaysInMonth() - presentDate) + " days left in month of December.");
+            default -> System.out.println("Invalid Inputs, please check and try again.");
+        }
+
     }
 }
 
