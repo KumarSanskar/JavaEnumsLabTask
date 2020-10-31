79ff169a53882887beeba3150b226bd073aa1de6
Kumar Sanskar <61062977+KumarSanskar@users.noreply.github.com>
2020-10-31 14:13:56 +0530

    made change to parameter value of OCTOBER.

--- src/MonthNames.java
+++ src/MonthNames.java
@@ -1,7 +1,7 @@
 import java.util.Scanner;
 
 public enum MonthNames {
-    JANUARY(31), FEBRUARY(29), MARCH(31), APRIL(30), MAY(31), JUNE(30), JULY(31), AUGUST(31), SEPTEMBER(30), OCTOBER(30), NOVEMBER(30), DECEMBER(31);
+    JANUARY(31), FEBRUARY(29), MARCH(31), APRIL(30), MAY(31), JUNE(30), JULY(31), AUGUST(31), SEPTEMBER(30), OCTOBER(31), NOVEMBER(30), DECEMBER(31);
 
 
     private final int numberOfDaysInMonth;
